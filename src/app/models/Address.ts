97b2c2edb5c0eb42b24676f2e12c9c645a23c33f
lyle delmolino <lyle.delmolino.tech@github.com>
2019-10-28 05:36:19 -0400

    ok

---
+++ src/app/models/Address.ts
@@ -0,0 +1,12 @@
+export class Address {
+  addressId?: number;
+  apartmentNumber?: string = "";
+  city?: string = "";
+  state?: string = "";
+  country?: string = "";
+  county?: string = "";
+  streetName?: string = "";
+  streetNumber?: string = "";
+  zipCode?: number;
+  zipPlusFour?: number;
+}
